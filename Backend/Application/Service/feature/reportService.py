9a213a76faf31a0008873f032ca1b09f53bcf123
Tonnwp <nameton2107@gmail.com>
2025-01-04 19:11:22 +0700

    [Add] add reports blueprint register

--- Backend/Application/Service/feature/reportService.py
+++ Backend/Application/Service/feature/reportService.py
@@ -1,18 +1,18 @@
 from Infrastructure.Repository.reportRepository import (
-    get_all_report,
+    get_all_reports,
     get_report_by_id,
     add_report,
     update_report,
     delete_report
 )
 
-def get_all_report_service():
-    return get_all_report()
+def get_all_reports_service():
+    return get_all_reports()
 
 def get_report_by_id_service(report_id):
     return get_report_by_id(report_id)
 
-def add_report_service(user_id, zone_id, report_status, report_type, report_message, report_image):
+def add_report_service(user_id, zone_id, report_status, report_type, report_message=None, report_image=None):
     return add_report(user_id, zone_id, report_status, report_type, report_message, report_image)
 
 def update_report_service(report_id, data):
